90fd46dd95676be5041618137d9d0f560682f244
ted-jv <wkejrdldls@gmail.com>
2022-06-01 15:30:58 +0900

    지금 서버와 연결 안 됬지 문제 없음 기존 누나 코딩 강의 보고 만든 파일에 스파르타 강의 접목 시켜서 여기까지 만들었음 이제 누나 코딩으로 만든 더 디테일한 부분들을 스파르타 강의의 내용으로 뒤덮을 것 그 후에 서버와 연결 시도 해볼 것.

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,46 +1,37 @@
-import React, { useState } from "react"
-import './Home.css';
+import React, { useState } from "react";
+import "./Home.css";
 import { useSelector } from "react-redux";
 import Box from "./Box";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 import { Container, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import App from "../firebase";
-
-
 
 function Home() {
+  const contents_list = useSelector((state) => state.box_contents_list);
 
-    const contents_list = useSelector((state) => state.box_contents_list);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const goto_detail = () => {
+    navigate("/Detail");
+  };
 
-    const goto_detail = () => {
-        navigate("/Detail")
-    }
-
-    return (
-        <div>
-            <Navbar />
-            <Container className="container">
-                <Row className="Row">
-
-                    {contents_list.map((item) => (
-                        <Col lg={4}>
-                            <Box item={item} />
-                        </Col>
-                    ))}
-
-                </Row>
-            </Container >
-            <div className="plusbutton" onClick={goto_detail}>
-                +
-            </div>
-        </div >
-
-    );
+  return (
+    <div>
+      <Navbar />
+      <Container className="container">
+        <Row className="Row">
+          {contents_list.map((item) => (
+            <Col lg={4}>
+              <Box item={item} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <div className="plusbutton" onClick={goto_detail}>
+        +
+      </div>
+    </div>
+  );
 }
 
-
-
-export default Home
+export default Home;
